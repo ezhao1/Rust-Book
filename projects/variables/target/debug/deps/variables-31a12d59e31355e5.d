ada54644b24e158041f407e8b91e563960d4fd3a
Ethan Zhao <ethandanielzhao@gmail.com>
2025-09-07 17:57:53 -0500

    Variables

---
+++ projects/variables/target/debug/deps/variables-31a12d59e31355e5.d
@@ -0,0 +1,5 @@
+/Users/ethanzhao/code/RustBook/projects/variables/target/debug/deps/variables-31a12d59e31355e5.d: src/main.rs
+
+/Users/ethanzhao/code/RustBook/projects/variables/target/debug/deps/variables-31a12d59e31355e5: src/main.rs
+
+src/main.rs:
---
+++ projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/ethanzhao/code/RustBook/projects/variables/target/debug/variables: /Users/ethanzhao/code/RustBook/projects/variables/src/main.rs
